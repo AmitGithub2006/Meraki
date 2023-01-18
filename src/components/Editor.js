90051487291894378d04f5336c927f5c36c7a68b
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2023-01-18 17:28:25 +0530

    added ide

---
+++ src/components/Editor.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+
+export default function Editor(props) {
+  const { language, displayName, value, onChange } = props;
+  const [open, setOpen] = useState(true);
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if ((e.ctrlKey || e.metaKey) && e.key === "v") {
+        e.preventDefault();
+      }
+    });
+  }, []);
+
+  return (
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
+      </div>
+      <div
+        onCut={(e) => e.preventDefault()}
+        onCopy={(e) => e.preventDefault()}
+        onContextMenu={(e) => e.preventDefault()}
+        // onPaste={(e) => e.preventDefault()}
+      >
+        <ControlledEditor
+          onBeforeChange={handleChange}
+          value={value}
+          className="code-mirror-wrapper"
+          options={{
+            lineWrapping: true,
+            lint: true,
+            mode: language,
+            theme: "material",
+            lineNumbers: true,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
